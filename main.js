29a9411bc05fead1729ae5171f9870d1e11e9b8e
xcobaltfury3000 <39102705+xcobaltfury3000@users.noreply.github.com>
2021-05-02 18:35:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -32,3 +32,30 @@ Particle.prototype.draw = function(){
     ctx.arc(this.x, this.y, this.size, Math.PI * 2, false);
     ctx.fillstyle
 }
+
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  // Remove links that don't actually link to anything
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+
+        var scrollTop = target.offset().top - $('.sticky-top').height();
+
+        $('html, body').animate({
+          scrollTop: scrollTop
+        }, 1000)
+      }
+    }
+});
